55a3b9496daf9bdcf03bdd32641618c05a2ed853
NECTEC_zChatchawalS <zchatchawals@pttep.com>
2022-05-26 10:09:34 +0700

    base model

--- app/main.py
+++ app/main.py
@@ -1,4 +1,32 @@
 from fastapi import FastAPI, Header,Form, Body
+from pydantic import BaseModel
+
+
+class ConversationUnit(BaseModel):
+    text: str
+    intent: str
+    entities: list
+    confidence: float
+    response: str
+    context: dict
+
+
+class Service(BaseModel):
+    name: str
+    description: str
+    version: str
+    url: str
+    status: str
+
+class incoming_message(BaseModel):
+    session_id: str
+    text: str
+    context: dict
+
+class outgoing_message(BaseModel):
+    text: str
+    context: dict
+
 
 
 app = FastAPI(
@@ -12,31 +40,61 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.post("/conversation",tags=["Conversation Unit"])
+def create_conversation(conversation_unit: ConversationUnit):
+    res = {
+        "status": "success",
+    }
+    return res   
+
 @app.get("/conversation",tags=["Conversation Unit"])
-def get_conversation():
+def get_conversation(cid: str):
     return {"Hello": "World"}
 
-@app.post("/conversation",tags=["Conversation Unit"])
-def update_conversation():
-    return {"Hello": "World"}   
+@app.put("/conversation",tags=["Conversation Unit"])
+def update_conversation(conversation_unit: ConversationUnit):
+    res = {
+        "status": "success",
+    }
+    return res 
 
 @app.delete("/conversation",tags=["Conversation Unit"])
 def delete_conversation(cid:str=""):
-    return {"status": True}    
+    res = {
+        "status": "success",
+    }
+    return res   
+
+
+@app.post("/service",tags=["Service"])
+def create_service(service:Service):
+
+    res = {
+        "status": "success",
+    }
+    return res 
 
 @app.get("/service",tags=["Service"])
 def get_service():
     return {"Hello": "World"}
 
-@app.post("/service",tags=["Service"])
-def update_service():
-    return {"Hello": "World"}   
+@app.put("/service",tags=["Service"])
+def update_service(service:Service):
+    
+    res = {
+        "status": "success",
+    }
+    return res 
 
 @app.delete("/service",tags=["Service"])
 def delete_service(sid:str=""):
-    return {"status": True}    
+
+    res = {
+        "status": "success",
+    }
+    return res  
 
 
-@app.get("/chat",tags=["Chat"])
-def chat(session_id:str="",incoming_message:str=""):
+@app.get("/chat",tags=["Chat"],response_model=outgoing_message)
+def chat(inc_msg: incoming_message):
     return {"Hello": "World"}
